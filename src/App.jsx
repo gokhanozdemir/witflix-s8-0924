11f520683a7b6d3f17e12314598f25c607e908f1
Gökhan <gn.ozdemir@gmail.com>
2024-11-05 11:37:09 +0300

    form elemanlarını taslaklar

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,30 @@ function App() {
         <div className="container-lg">NEETFLIK</div>
       </header>
       <main className="flex column gap-m main-container" >
-        <div className="container-lg">
+        <div className="container-lg flex column">
           <form onSubmit={handleSubmit}>
+
+
+            <label htmlFor="firstname">First Name</label>
+            <input type="text" name="firstname" id="firstname" />
+            <label htmlFor="lastname">Last Name</label>
+            <input type="text" name="lastname" id="lastname" />
+            <label htmlFor="creditcard">Credit Card Number</label>
+            <input type="text" name="creditcard" id="creditcard" />
+            <label htmlFor="cvc">CVC</label>
+            <input type="text" name="cvc" id="cvc" />
+            <label htmlFor="exp_month">Month</label>
+            <input type="text" name="exp_month" id="exp_month" />
+            <label htmlFor="exp_year">Year</label>
+            <input type="text" name="exp_year" id="exp_year" />
+            <label htmlFor="address">Address</label>
+            <input type="text" name="address" id="address" />
+            <label htmlFor="zipcode">Zip Code</label>
+            <input type="text" name="zipcode" id="zipcode" />
+            <label htmlFor="city">City</label>
+            <input type="text" name="city" id="city" />
+            <label htmlFor="district">District</label>
+            <input type="text" name="district" id="district" />
 
 
 
