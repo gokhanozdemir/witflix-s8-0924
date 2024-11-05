22cb27e4bd23bc9b210abd322853465e57b214cd
Gökhan <gn.ozdemir@gmail.com>
2024-11-05 11:25:58 +0300

    form elemanını taslaklar

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,28 @@
 import { useState } from "react"
-import Profile from "./components/Profile"
-import './Form.css'
-const APIDataStatic = [
-  {
-    "createdAt": "2024-10-17T03:53:51.456Z",
-    "name": "Alyssa Reinger IV",
-    "avatarUrl": "https://api.multiavatar.com/1212.svg",
-    "id": "1"
-  },
-  {
-    "createdAt": "2024-10-17T07:00:18.480Z",
-    "name": "Dr. Sharon Howe",
-    "avatarUrl": "https://api.multiavatar.com/740.svg",
-    "id": "2"
-  },
-  {
-    "createdAt": "2024-10-16T19:29:04.974Z",
-    "name": "Lila Murray",
-    "avatarUrl": "https://api.multiavatar.com/128.svg",
-    "id": "3"
-  },
-  {
-    "createdAt": "2024-10-16T19:54:13.416Z",
-    "name": "Mildred Grimes",
-    "avatarUrl": "https://api.multiavatar.com/1135.svg",
-    "id": "4"
-  }
-]
 
 
 function App() {
-  const [users, setUsers] = useState(APIDataStatic);
-  const [form, setForm] = useState({ username: "" });
-
-  const handleAddUser = (newUser) => {
-    setUsers([newUser, ...users])
-  }
+  const [formData, setFormData] = useState({});
 
   const handleChange = (event) => {
     // console.log(event)
     const newUser = {
-      ...form,
+      ...formData,
       [event.target.name]: event.target.value
     }
 
-    setForm(newUser)
+    setFormData(newUser)
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("formdata", formData)
     const submittedUser = {
-      name: form.username,
+      name: formData.username,
       id: Date.now(),
-      avatarUrl: `https://api.multiavatar.com/${form.username}.svg`
+      avatarUrl: `https://api.multiavatar.com/${formData.username}.svg`
     }
-    setForm({ username: "" })
-    handleAddUser(submittedUser)
+    setFormData({})
   }
 
   return (
@@ -64,55 +31,20 @@ function App() {
         <div className="container-lg">NEETFLIK</div>
       </header>
       <main className="flex column gap-m main-container" >
-        <div className="container-s">
+        <div className="container-lg">
           <form onSubmit={handleSubmit}>
-            {form.username}
-            <div className="input-group">
-              <label htmlFor="username">Username</label>
-              <input value={form.username} onChange={handleChange} type="text" id="username" name="username" placeholder="Kullanıcı Adı" />
-            </div>
 
-            <button type="submit">Add User</button>
+
+
+            <button type="submit">Pay Now</button>
+            <button type="button">Cancel</button>
+            <button type="button">Reset Form</button>
           </form>
         </div>
 
-        <div className="container-lg flex wrap center gap-s">
-          {users.map((user) => (
-            <Profile
-              key={user.id}
-              info={user}
-            />
-          ))}
-        </div>
+
       </main>
-      <footer>
-        <div className="container-lg flex between">
-          <div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-          </div>
-          <div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-          </div>
-          <div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-          </div>
-          <div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-            <div className="">divdivdiv</div>
-          </div>
-        </div>
-      </footer>
+
     </div>
   )
 }
